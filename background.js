1d6e3f5d2fbf1de8caff04756f1147e53a58b5fd
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-15 08:32:28 +0200

    added basic extension with pop up window and current url detection

---
+++ background.js
@@ -0,0 +1,24 @@
+// list of urls
+var urls = [
+    "https://www.casino.fr/ecommerce/GC-catalog/fr/WE63936/?moderetrait=Z4",
+    "http://www.yahoo.com",
+    "http://www.bing.com"
+];
+
+// dictionary of urls and corresponding div class
+var urlDivMap = {
+    "https://www.casino.fr/ecommerce/GC-catalog/fr/WE63936/?moderetrait=Z4": "product-item__inner",
+    "http://www.yahoo.com": "yahoo",
+    "http://www.bing.com": "bing"
+};
+
+
+// function to get the url of the current tab
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let url = tabs[0].url;
+    if (urls.includes(url)) {
+        // change background color to red
+        document.body.style.backgroundColor = "red";
+        console.log("This is a test");
+    }
+});
