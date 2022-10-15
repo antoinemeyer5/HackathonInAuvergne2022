fe7c74b8c6a7dd39d92d0a43f06f97f88e2b3985
antoinemeyer5 <antoine.meyer54@gmail.com>
2022-10-15 18:04:23 +0200

    modif html with indicator score

--- filter.js
+++ filter.js
@@ -9,5 +9,32 @@ for (let i = 0; i < list_products.length; i++) {
     url_products.push(list_products.item(i).href);
 }
 
+// call Taha's API
+
+// recup response : 88 / 100
+// var list_res = [18, 88, 100, 50, 37];
+
+// modify dom page for display our score
+let list_product_to_modify = document.getElementsByClassName("product-thumbnail__header");
+
+//let para = document.createElement("p");
+//let textnode = document.createTextNode("Water");
+//para.appendChild(textnode);
+for (let i = 0; i < list_product_to_modify.length; i++) {
+    console.log(list_product_to_modify.item(i));
+    list_product_to_modify.item(i).style.backgroundColor = "red";
+    // list_product_to_modify.item(i).backgroundColor = "red";
+
+}
+
+
+/*for (let i = 0; i < list_products.length; i++) {
+    //list_products.appendChild(para);
+}*/
+
+
+
+
+
 // debug
-console.log(url_products);
+// console.log(url_products);
