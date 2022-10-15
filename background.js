525cd6c593e6fac13b583f771ba6b874fd6fd10f
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-16 01:21:38 +0200

    integrating basket items into popup

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
 // Auchan url
 var url_auchan = "https://www.auchan.fr";
 
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:8000/basket", true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(null);
+
+// get response
+xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        document.body.style.backgroundColor = "red";
+        // transfrom response to json
+        var response = JSON.parse(this.responseText);
+        // get basket
+        for (let i = 0; i < response.prices.length; i++) {
+
+            let p = document.createElement("p");
+            
+            p.append("Basket: " + response.brands[i] + " " + response.prices[i]);
+            document.body.append(p);
+        }
+    }
+};
+
+
 // function to get the url of the current tab
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     let url = tabs[0].url;
@@ -11,5 +34,9 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             target: { tabId: tabs[0].id },
             files: ['filter.js']
         });
+
+
+        // get basket from localhost:8000/basket
+
     }
 });
