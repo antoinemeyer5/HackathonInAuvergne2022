d70b2c808b75b3c29bf2843e5a4d901721bcfe48
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-15 14:12:08 +0200

    api responds to quests/:id and products/:id

--- API.js
+++ API.js
@@ -18,12 +18,32 @@ app.get('/', (req, res) => {
 const fs = require('fs');
 const file = fs.readFileSync('db.json', 'utf8');
 
+const quests = fs.readFileSync('questsData.json', 'utf8');
+
 // read db
 const db = JSON.parse(file);
+const questsData = JSON.parse(quests);
 
 
-app.get('/products', (req, res) => {
+app.get('/products/:id', (req, res) => {
+    // get id
+    const id = req.params.id;
+    console.log("Requesting product", id);
     console.log(req.query);
-    res.send(db);
+    // send product with code id
+    const product = db.products.find(product => product.code == id);
+    res.send(product);
+}
+);
+
+
+app.get('/quests/:id', (req, res) => {
+    // get id
+    const id = req.params.id;
+    console.log("Requesting quest", id);
+    console.log(req.query);
+    // send product with code id
+    const quest = questsData.quests.find(quest => quest.id == id);
+    res.send(quest);
 }
 );
