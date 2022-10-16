f7132c47baa755e84029ec4bf11663e8323a3c9f
antoinemeyer5 <antoine.meyer54@gmail.com>
2022-10-16 10:25:54 +0200

    total panier et ethicagnotte

--- background.js
+++ background.js
@@ -12,42 +12,50 @@ xhr.setRequestHeader('Content-Type', 'application/json');
 xhr.send(null);
 // document.body.style.backgroundColor = "red";
 
-
 //quest
 let quest = document.createElement("p");
 quest.append("Acheter 3 produits ayant une note supérieure à 8");
 document.getElementsByClassName("conteneur_quetes")[0].append(quest);
 
-
+//
 quest = document.createElement("p");
 quest.append("Acheter 10 produits provenant de France");
 document.getElementsByClassName("conteneur_quetes")[0].append(quest);
 
-
 // trait horizontal
 document.body.append(document.createElement("hr"));
 
-// add ethic'score
-let ethic_score = document.createElement("p");
-ethic_score.append("Votre Ethic'score : 0.00€");
-document.body.append(ethic_score);
-
-
 // get response
 var response = xhr.responseText;
+let somme_panier;
 
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         // transfrom response to json
         response = JSON.parse(this.responseText);
+
+        // somme du panier
+        somme_panier = 0;
+        
         // add data in display html
         // get basket
         for (let i = 0; i < response.prices.length; i++) {
             let details = document.createElement("p");
             details.append(response.brands[i] + " : " + response.prices[i]);
+            // total panier
+            somme_panier = somme_panier + parseInt(response.prices[i].slice(0, response.prices[i].length-1));
             // add in div with conteneur_details class
             document.getElementsByClassName("conteneur_details")[0].append(details);
         }
+
+        // coef
+        somme_panier = somme_panier * 0.06;
+
+        // add ethic'score
+        let ethic_score = document.createElement("p");
+        ethic_score.append("Votre Ethic'cagnotte : " + somme_panier + " €");
+        document.getElementsByClassName("conteneur_ecoscore")[0].append(ethic_score);
+
     }
 };
 
